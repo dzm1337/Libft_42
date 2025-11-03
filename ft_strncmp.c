8e4a50893236417a0d1dd295bb4325affdd32fd1
Diogo Marcelo De Paulo Triles <dde-paul@a2-r3-p6.s19.be>
2025-11-03 19:21:43 +0100

    a

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dde-paul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 15:38:43 by dde-paul          #+#    #+#             */
-/*   Updated: 2025/10/31 23:42:10 by dde-paul         ###   ########.fr       */
+/*   Updated: 2025/11/03 19:20:37 by dde-paul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	while ((s1[i] && s2[i]) && (size_t)i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			retur		n (s1[i] - s2[i]);
 		i++;
 	}
 	return (s1[i] - s2[i]);
